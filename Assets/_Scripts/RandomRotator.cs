ec3f48ae987fa986d58e5c5eabbaf98d3dc48f55
white <white@xiaoman.cn>
2016-03-22 14:14:00 +0800

    seven

---
+++ Assets/_Scripts/RandomRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	
+	public float maxRotation;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitCircle * maxRotation;
+	}
+}
