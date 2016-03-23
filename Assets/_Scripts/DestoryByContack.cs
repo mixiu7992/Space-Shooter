7d4b3aff01f9755f1d893e3620ae2c8790d93946
white <white@xiaoman.cn>
2016-03-23 12:58:07 +0800

    bolt explosion

--- Assets/_Scripts/DestoryByContack.cs
+++ Assets/_Scripts/DestoryByContack.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestoryByContack : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+public class DestoryByContack : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "Boundary"){
+			return;
+		}
+		if(explosion != null){
+			Instantiate (explosion, transform.position, transform.rotation);	
+		}
+		if(other.tag == "Player"){
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Debug.Log(other.name);
+		Destroy(other.gameObject);
+		Destroy (gameObject);
 	}
 }
