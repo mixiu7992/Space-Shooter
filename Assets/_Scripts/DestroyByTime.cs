bcd41ea4b51eb58a4d92f6b75420a198c25b3de6
white <white@xiaoman.cn>
2016-03-24 12:21:57 +0800

    bolt destroy

---
+++ Assets/_Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float destroyWait;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, destroyWait);
+	}
+}
